bd06f2e153cfe73731df1af614395fb3be19ba94
arn-dad <dadayanaa@yahoo.com>
2024-10-07 15:37:51 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function createInfoList(info) {
+  const ul = document.createElement('ul');
+
+  for (const [key, value] of Object.entries(info)) {
+    console.log(`${key}: ${value}`);
+
+    const li = document.createElement("li");
+    li.innerText = `${key}: ${value}`
+
+    ul.appendChild(li);
+  }
+
+  document.body.appendChild(ul)
+}
+
+
+const userInfo = {
+  language: navigator.language,
+  name: navigator.userAgent
+}
+
+createInfoList(userInfo);
+
